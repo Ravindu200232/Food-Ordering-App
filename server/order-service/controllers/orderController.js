de0b149e2665945dbaf77b1e45466fb1af451d38
Ravindu Bandara Subasinha <it22098450@my.sliit.lk>
2025-04-20 22:04:23 +0530

    order
    order-->
    model
    controler
    route add get delete

--- server/order-service/controllers/orderController.js
+++ server/order-service/controllers/orderController.js
@@ -1,5 +1,5 @@
 import Order from "../models/order.js";
-import { checkAdmin, checkCustomer, checkHasAccount } from "./authController.js";
+import { checkAdmin, checkCustomer, checkHasAccount, checkRestaurant } from "./authController.js";
 
 export async function addOrder(req,res) {
 
@@ -9,17 +9,15 @@ export async function addOrder(req,res) {
     data.userId = req.user.id;
 
     if(checkHasAccount(req)){
-        if(checkAdmin(req) || checkCustomer(req)){
+        
             const newOrder = new Order(data)
             await newOrder.save();
             res.json({
                 message : "Order added successfully!"
             })
-        }else{
-            res.json({
-                message : "can't access this task"
-            })
-        }
+            return
+       
+       
     }else{
         res.status(404).json({
             message : "Please login first!"
@@ -34,4 +32,89 @@ export async function addOrder(req,res) {
 
     
 
+}
+
+export async function getOrder(req,res) {
+
+    try{
+
+       
+        if(checkHasAccount(req)){
+
+            if(checkAdmin(req)){
+               const result = await Order.find();
+               res.json(result)
+               return 
+            }
+
+            if(checkRestaurant(req) || checkCustomer(req)){
+
+                const result = await Order.find({
+                    userId : req.user.id 
+                })
+                res.json(result)
+                return
+            }
+
+            else{
+                res.status(401).json({
+                    message : "cant access this task"
+                })
+                return
+            }
+
+
+        }else{
+            res.status(401).json({
+                message : "cant access this task"
+            })
+            return
+        }
+
+    }catch(err){
+        res.status(500).json({
+            error : "Internal Server error" || err
+        })
+    }
+    
+}
+
+export async function deleteOrder(req,res) {
+
+    try{
+
+        const id = req.params.id;
+        if(checkHasAccount(req)){
+
+            if(checkAdmin(req)){
+                await Order.deleteOne({
+                    _id : id
+                })
+                res.json({
+                    message : "Order deleted successfully"
+                })
+                return
+            }else{
+
+                await Order.deleteOne({
+                    _id : id,
+                    userId : req.user.id
+                })
+                res.json({
+                    message : "Order deleted successfully"
+                })
+                return
+            }
+        }else{
+            res.status(401).json({
+                message : "Please login"
+            })
+            return
+        }
+    }catch(err){
+        res.status(500).json({
+            error : "Internal Server error" || err
+        })
+    }
+    
 }
