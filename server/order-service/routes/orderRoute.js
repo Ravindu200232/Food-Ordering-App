d2dcbaabdd45ee1df4f0a99c7c7acc42f389a7fd
Ravindu Bandara Subasinha <it22098450@my.sliit.lk>
2025-04-21 12:11:35 +0530

    sdf

--- server/order-service/routes/orderRoute.js
+++ server/order-service/routes/orderRoute.js
@@ -1,8 +1,10 @@
 import express from "express"
-import { addOrder } from "../controllers/orderController.js";
+import { addOrder, deleteOrder, getOrder } from "../controllers/orderController.js";
 
 const orderRoute = express.Router();
 
 orderRoute.post("/",addOrder);
+orderRoute.get("/",getOrder)
+orderRoute.delete("/delete/:id",deleteOrder)
 
 export default orderRoute
