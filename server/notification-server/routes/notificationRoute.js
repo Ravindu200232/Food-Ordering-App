a47df7d7dc5616c92b055b336e172a57c2b74a92
Ravindu Bandara Subasinha <it22098450@my.sliit.lk>
2025-04-27 23:14:17 +0530

    update

--- server/notification-server/routes/notificationRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { EmailSender } from '../controllers/notificationSender.js';
-
-const notificationRoute = express.Router();
-
-notificationRoute.post("/",EmailSender);
-
-export default notificationRoute
